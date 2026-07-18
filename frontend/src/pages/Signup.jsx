6dbacf36d0b68c9955a8c706a0dd4bcca2d5537b
Rakesh Sah <rakeshsah90346@gmail.com>
2026-07-18 17:45:45 +0530

    Debug API URL

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL =", API_URL);
 
 export default function Register() {
   const [formData, setFormData] = useState({
